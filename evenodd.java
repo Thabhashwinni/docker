3b5ce88f0a7f888608a0b83592739233009adc32
Thabhashwinni <kthabhashwinni@gmail.com>
2025-02-21 14:30:26 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,11 @@
+public class evenodd {
+    public static void main(String[] args) {
+        int number = 7;  // You can change this number to test with other values
+        
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+    }
+}
